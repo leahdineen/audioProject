022e856856abd2fa127ca34cbb4622e2b50baaba
Tyler Sanderson <tylerbtbam@gmail.com>
2016-03-10 20:36:56 -0500

    Integrated waveforms with UI. Fixed up OscillatorNode connectivity issues.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 (function(){
 
-var radios = document.forms["waveType"].elements["wave"];
-for(var i = 0, max = radios.length; i < max; i++) {
-    radios[i].onclick = function() {
-        console.log(this.value);
-    }
-}
-var synth = Object.create(Synth.prototype);
-synth.play();
+var synth = null;
 
+var init = function(){
+    synth = new Synth();
+
+    var radios = document.forms["waveType"].elements["wave"];
+    for(var i = 0, max = radios.length; i < max; i++) {
+        radios[i].onclick = function() {
+            synth.setWaveForm(this.value);
+        }
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function(event){
+    init();
 });
+
+document.addEventListener("keydown", function(event){
+    var key = String.fromCharCode(event.which || event.keyCode);
+    synth.play();
+});
+
+document.addEventListener("keyup", function(event){
+    synth.stop();
+});
+})();
--- js/synth.js
+++ js/synth.js
@@ -1,40 +1,42 @@
-/ * This is mainly a wrapper class to play Audio through
+/* This is mainly a wrapper class to play Audio through
     the Web Audio API using an OscillatorNode connected
     to an AudioContext.
 */
 function Synth(opts){
-    this.oscillator = null;
-    this.context = null;
     this.init(opts);
 }
 
 Synth.prototype.init = function(opts){
-
     // Cross browser compatibility for Web Audio API
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     this.context = new AudioContext();
 
-    this.oscillator = this.context.createOscillator();
-
     // Default to square wave at A4
     var defaultOpts = {type: 'square', frequency: 440};
-    Object.assign(opts || defaultOpts, this.oscillator);
-    this.oscillator.connect(this.context.destination);
+    this.opts = opts || defaultOpts;
 };
 
 Synth.prototype.setWaveForm = function(type){
-    this.oscillator.type = type;
+    this.opts.type = type;
 };
 
 Synth.prototype.setFrequency = function(freq){
     // In Hz
-    this.oscillator.frequency = freq;
+    this.opts.frequency = freq;
 };
 
 Synth.prototype.play = function(delay){
+
+    this.oscillator = this.context.createOscillator();
+    // Set options up
+    for (var o in this.opts){
+        this.oscillator[o] = this.opts[o];
+    }
+    this.oscillator.connect(this.context.destination);
     this.oscillator.start(delay || 0);
 };
 
 Synth.prototype.stop = function(delay){
     this.oscillator.stop(delay || 0);
+    this.oscillator.disconnect(this.context.destination);  
 };
