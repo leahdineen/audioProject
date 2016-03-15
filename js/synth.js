c142755b914f525bb5bfb054f5037600f1d443ee
ajvaloppi <amanda.valoppi@gmail.com>
2016-03-15 13:40:53 -0400

    Random channel data reverb

--- js/synth.js
+++ js/synth.js
@@ -62,6 +62,26 @@ Synth.prototype.play = function(freq){
         voice.volumeNode = this.context.createGain();
         voice.volumeNode.gain.setValueAtTime(this.volume, this.context.currentTime);
 
+        // Reverb
+        voice.convolver = this.context.createConvolver();
+        var buffer = this.context.createBuffer(2, this.context.sampleRate * 2.0, this.context.sampleRate);
+
+        var source = this.context.createBufferSource();
+        source.buffer = buffer;
+        
+
+        for (var channel = 0; channel < 2; channel ++) {
+            // This gives us the actual array that contains the data
+           var nowBuffering = buffer.getChannelData(channel);
+           for (var i = 0; i < this.context.sampleRate * 2.0; i++) {
+             // Math.random() is in [0; 1.0]
+             // audio needs to be in [-1.0; 1.0]
+             nowBuffering[i] = Math.random() * 2 - 1;
+           }
+        }
+
+        voice.convolver.buffer = source.buffer;
+
         // Stereo Pan
         voice.panNode = this.context.createStereoPanner();
         voice.panNode.pan.value = this.pan;
@@ -69,14 +89,21 @@ Synth.prototype.play = function(freq){
         voice.oscillator.start();
         voice.oscillator.connect(voice.gainNode);
         voice.gainNode.connect(voice.volumeNode);
+        voice.volumeNode.connect(voice.convolver);
         voice.volumeNode.connect(voice.panNode);
         voice.panNode.connect(this.context.destination);
+
+        voice.volumeNode.connect(voice.convolver)
+        voice.convolver.connect(this.context.destination);
 
         // Trigger our ADSR amplitude envelope
         voice.amplitudeEnv = new Envelope(this.envelopeOpts, this.context);
         voice.amplitudeEnv.connect(voice.gainNode.gain);
         voice.amplitudeEnv.trigger();
+
+        
     }
+
 
 };
 
